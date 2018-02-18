401a9a53f5d6a10bd5e8db27cc50b0ef31745f01
Julien Tanay <julien.tanay@gmail.com>
2018-02-18 23:58:31 +0100

    add some zsh init script

---
+++ init/zsh.sh
@@ -0,0 +1,4 @@
+cd $HOME/.oh-my-zsh/custom/plugins
+git clone https://github.com/zsh-users/zsh-syntax-highlighting
+git clone https://github.com/zsh-users/zsh-autosuggestions
+git clone https://github.com/zsh-users/zsh-completions
