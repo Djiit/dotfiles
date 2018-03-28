e7c717e813aab3b286bb093ffbde4b81176dfd0d
Julien Tanay <jtanay@MacBook-Pro-de-Amaury-2.local>
2018-03-28 14:15:13 +0200

    add macos init

---
+++ init/10_macos_common.sh
@@ -0,0 +1,9 @@
+# Install brew
+/usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+
+# Install brew cask
+brew tap phinze/cask
+brew install brew-cask
+
+# Install common packages from brew Cask
+brew cask install docker node
