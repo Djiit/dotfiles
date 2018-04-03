cf2a778a3534bb430acee60b724c9c1d181c81a8
Julien Tanay <jtanay@ooreka.fr>
2018-04-03 10:48:23 +0200

    update config for macos platform

--- init/10_macos_common.sh
+++ init/10_macos_common.sh
@@ -1,9 +1,15 @@
 # Install brew
 /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
 
-# Install brew cask
+# Install common packages from brew
+brew install node z yarn awscli packer tfenv jq
+
+# Install brew Cask
 brew tap phinze/cask
 brew install brew-cask
 
 # Install common packages from brew Cask
-brew cask install docker node
+brew cask install vlc docker node thefuck visual-studio-code
+
+# Clean-Up
+brew clean-up --force
