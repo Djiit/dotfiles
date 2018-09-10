6b4b166c0722a296f4a2d6e1ab34ee4ee1903546
Julien Tanay <jtanay@ooreka.fr>
2018-09-10 16:52:41 +0200

    feat: add fzf, ag, noti, bat

--- init/10_macos_common.sh
+++ init/10_macos_common.sh
@@ -2,7 +2,9 @@
 /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
 
 # Install common packages from brew
-brew install node z yarn awscli packer tfenv jq coreutils
+brew install node z yarn awscli packer tfenv jq coreutils prettyping bat fzf tldr noti the_silver_searcher
+
+$(brew --prefix)/opt/fzf/install
 
 # Install brew Cask
 brew tap phinze/cask
@@ -13,3 +15,5 @@ brew cask install vlc docker node visual-studio-code filezilla cheatsheet specta
 
 # Clean-Up
 brew clean-up --force
+
+alias ping='prettyping --nolegend'
