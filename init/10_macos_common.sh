6ec6133e26e3f79c9d00b6b529c440aee2f552f7
Julien Tanay <julien.tanay@gmail.com>
2020-03-12 18:07:38 +0100

    feat: add eksctl

--- init/10_macos_common.sh
+++ init/10_macos_common.sh
@@ -3,6 +3,10 @@
 
 # Install common packages from brew
 brew install node z yarn awscli jq coreutils prettyping bat fzf tldr noti the_silver_searcher derailed/k9s/k9s
+
+brew tap weaveworks/tap
+brew install weaveworks/tap/eksctl
+
 $(brew --prefix)/opt/fzf/install
 
 # Install common packages from brew Cask
