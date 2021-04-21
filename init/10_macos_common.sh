7279f727c1b818c95c9d91c1bd74c02a92b475bc
Julien Tanay <julien.tanay@gmail.com>
2021-04-21 15:24:41 +0200

    feat(mac): add sops / aws-vault

--- init/10_macos_common.sh
+++ init/10_macos_common.sh
@@ -5,7 +5,7 @@
 brew install zsh jq prettyping bat fzf tldr noti the_silver_searcher
 
 # Install dev/ops packages from brew
-brew install awscli helm derailed/k9s/k9s yarn coreutils
+brew install awscli helm derailed/k9s/k9s yarn coreutils sops 
 
 brew tap weaveworks/tap && brew install weaveworks/tap/eksctl
 brew tap heroku/brew && brew install heroku
@@ -14,7 +14,10 @@ brew tap heroku/brew && brew install heroku
 $(brew --prefix)/opt/fzf/install
 
 # Install common packages from brew Cask
-brew install --cask vlc docker visual-studio-code cheatsheet joplin maccy keepingyouawake
+brew install --cask vlc joplin maccy keepingyouawake
+
+# Install dev/ops packages from brew Cask
+brew install --cask docker aws-vault visual-studio-code cheatsheet 
 
 # Clean-Up
 brew clean-up --force
