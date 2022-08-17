c773a3ff1de5deec919542e98cd80afd44ede238
Julien Tanay <julien.tanay@gmail.com>
2022-08-17 18:23:26 +0200

    fix(mac): update docker install

--- init/10_macos_common.sh
+++ init/10_macos_common.sh
@@ -5,7 +5,7 @@
 brew install zsh jq prettyping bat fzf tldr noti the_silver_searcher fx syncthing
 
 # Install dev/ops packages from brew
-brew install awscli helm derailed/k9s/k9s yarn coreutils sops oras
+brew install awscli docker docker-compose kubernetes-cli lima colima helm derailed/k9s/k9s yarn coreutils sops oras
 
 brew tap weaveworks/tap && brew install weaveworks/tap/eksctl
 brew tap heroku/brew && brew install heroku
@@ -13,11 +13,15 @@ brew tap heroku/brew && brew install heroku
 # post-install for fzf
 $(brew --prefix)/opt/fzf/install
 
+# post-install for docker-compose
+mkdir -p ~/.docker/cli-plugins
+ln -sfn $(brew --prefix)/opt/docker-compose/bin/docker-compose ~/.docker/cli-plugins/docker-compose
+
 # Install common packages from brew Cask
 brew install --cask vlc joplin maccy keepingyouawake monitorcontrol thunderbird
 
 # Install dev/ops packages from brew Cask
-brew install --cask docker aws-vault visual-studio-code cheatsheet 
+brew install --cask aws-vault visual-studio-code cheatsheet 
 
 # Clean-Up
 brew clean-up --force
