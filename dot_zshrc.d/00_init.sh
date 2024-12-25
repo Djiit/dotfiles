bea04257de55da3eb1d4831896d5bc9cde26d948
Julien Tanay <julien.tanay@doctolib.com>
2024-12-25 16:50:04 +0100

    feat: add devcontainers, zshrc.d

---
+++ dot_zshrc.d/00_init.sh
@@ -0,0 +1,66 @@
+# starship
+eval "$(starship init zsh)"
+
+# autocompletion
+source $(brew --prefix)/share/zsh-autosuggestions/zsh-autosuggestions.zsh
+
+# zoxide
+eval "$(zoxide init zsh)"
+
+# direnv
+eval "$(direnv hook zsh)"
+
+# Go
+export GOPATH=$HOME/golang
+export GOROOT=/opt/homebrew/opt/go/libexec
+export PATH=$PATH:$GOPATH/bin
+export PATH=$PATH:$GOROOT/bin
+
+# Moon
+export PATH="$HOME/.moon/bin:$PATH"
+
+# Copilot
+eval "$(github-copilot-cli alias -- "$0")"
+
+# Tilt
+alias tilt=/opt/homebrew/bin/tilt
+
+# GPG
+export GPG_TTY=$(tty)
+
+# Krew
+export PATH="${KREW_ROOT:-$HOME/.krew}/bin:$PATH"
+# bun completions
+[ -s "/Users/julien.tanay/.bun/_bun" ] && source "/Users/julien.tanay/.bun/_bun"
+
+# bun
+export BUN_INSTALL="$HOME/.bun"
+export PATH="$BUN_INSTALL/bin:$PATH"
+
+# Created by `pipx` on 2023-10-25 14:53:02
+export PATH="$PATH:/Users/julien.tanay/.local/bin"
+test -e "${HOME}/.iterm2_shell_integration.zsh" && source "${HOME}/.iterm2_shell_integration.zsh"
+
+export MICRO_TRUECOLOR=1
+eval "$(/Users/julien.tanay/.local/bin/mise activate zsh)"
+
+# Added by LM Studio CLI (lms)
+export PATH="$PATH:/Users/julien.tanay/.cache/lm-studio/bin"
+
+# pnpm
+export PNPM_HOME="/Users/julien.tanay/Library/pnpm"
+case ":$PATH:" in
+  *":$PNPM_HOME:"*) ;;
+  *) export PATH="$PNPM_HOME:$PATH" ;;
+esac
+# pnpm end
+
+# Fubectl
+[ -f $HOME/fubectl.source ] && source $HOME/fubectl.source
+
+# Android Studio
+export ANDROID_HOME=$HOME/Library/Android/sdk
+export ANDROID_NDK_ROOT=/Users/julien.tanay/Library/Android/sdk/ndk/26.1.10909125
+export PATH=$PATH:$ANDROID_HOME/emulator
+export PATH=$PATH:$ANDROID_HOME/platform-tools
+export JAVA_HOME=/Applications/Android\ Studio.app/Contents/jbr/Contents/Home
